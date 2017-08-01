c6941bf460812492ca0250609ee9d0f1680d39da
Stefan Ilie <stefan.ilie@schindler.com>
2017-08-01 11:40:57 +0300

    add some views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 # maping the /index url
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name="results"),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.results, name="vote")
 ]
+
