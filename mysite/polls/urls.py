d593a64f93d388e25d8cb952f36a16a85ad45153
Stefan Ilie <stefan.ilie@schindler.com>
2017-08-02 11:40:14 +0300

    refactor all views using generic django ones

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,9 +7,9 @@ app_name = 'polls'
 
 # maping the urls
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name="results"),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.results, name="vote")
+    url(r'^$', views.IndexView.as_view(), name="index"),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name="detail"),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name="results"),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name="vote")
 ]
 
