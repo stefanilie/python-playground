c6395f91b375182bb4aff91e3e18e9c288b1d4fd
Stefan Ilie <stefan.ilie@schindler.com>
2017-08-08 17:45:15 +0300

    managed to make connection to mongodb available

--- hml/howmanyleft/models.py
+++ hml/howmanyleft/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from djangotoolbox.fields import ListField, EmbeddedModelField
 # Create your models here.
+
+class Car(models.Model):
+    marca = models.CharField(max_length=90)
+    modele = ListField(EmbeddedModelField('Modele'))
+
+class Modele(models.Model):
+    tip = models.CharField(max_length=90)
+    denumire = models.CharField(max_length=90)
+    categorie = models.CharField(max_length=90)
+    judet_bucati = ListField()
