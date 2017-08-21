e2f501df512a216acb4377ca9f6e225f7eba3d29
Stefan Ilie <stefan.ilie@schindler.com>
2017-08-21 17:45:40 +0300

    small clean

--- website/music/views.py
+++ website/music/views.py
@@ -1,17 +1,11 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album
 
 def index(request):
     all_albums = Album.objects.all()
-    context={ "all_albums": all_albums }
-    return render(request, 'music/index.html', context)
+    return render(request, 'music/index.html', { "all_albums": all_albums })
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-        context={ 'album': album }
-    except Album.DoesNotExist:
-        raise Http404("Item does not exist")
-    return render(request, 'music/detail.html', context)
+    album=get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album': album})
 
