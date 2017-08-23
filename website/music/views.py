cd5ce3b8090f6a878e35f5d3fbbf00974663254b
Stefan Ilie <stefan.ilie@schindler.com>
2017-08-23 11:34:56 +0300

    add favourite method by muself

--- website/music/views.py
+++ website/music/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Album
+from .models import Album, Song
 
 def index(request):
     all_albums = Album.objects.all()
@@ -9,3 +9,8 @@ def detail(request, album_id):
     album=get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
 
+def favorite(request, song_id):
+    song = get_object_or_404(Song, pk=song_id)
+    song.is_favourite = True
+    song.save()
+    return render(request, 'music/detail.html', {"album": song.album.id})
