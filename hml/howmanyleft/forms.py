6f28d788ce7d0efd77c26bf5f1ac7baeb96f10bc
Stefan Ilie <stefan.ilie@schindler.com>
2017-08-29 16:40:56 +0300

    started hml from scratch

--- hml/howmanyleft/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import (
-    Form, CharField, Textarea, PasswordInput, ChoiceField, DateField,
-    ImageField, EmailField,
-)
-
-class SearchForm(Form):
-    search_term = CharField(max_length=50)
--- hml/howmanyleft/views.py
+++ hml/howmanyleft/views.py
@@ -1,21 +1,6 @@
-# Create your views here.
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
-from django.core.urlresolvers import reverse
-from django.views import generic
-from django.template import loader
+from django.shortcuts import render
 
-from howmanyleft.forms import (
-    SearchForm,
-)
-
-def index(request):
-   if request.method == "GET":
-       form = SearchForm()
-       context = {
-           'form': form,
-       }
-       return render(request, 'howmanyleft/index.html', context)
+# Create your views here.
