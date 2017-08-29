9d789b8653f7875db60139a857182edd39a22cc6
Stefan Ilie <stefan.ilie@schindler.com>
2017-08-29 16:16:28 +0300

    changed from mongo to mysql

--- hml/hml/settings.py
+++ hml/hml/settings.py
@@ -75,10 +75,10 @@ WSGI_APPLICATION = 'hml.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default' : {
-      'ENGINE' : 'django_mongodb_engine',
-      'NAME' : 'my_database'
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
--- hml/howmanyleft/models.py
+++ hml/howmanyleft/models.py
@@ -3,12 +3,18 @@ from django.db import models
 from djangotoolbox.fields import ListField, EmbeddedModelField
 # Create your models here.
 
-class Car(models.Model):
+class Brand(models.Model):
     marca = models.CharField(max_length=90)
     modele = ListField(EmbeddedModelField('Modele'))
 
-class Modele(models.Model):
+class Model(models.Model):
+    brand = models.ForeignKeyField(Brand, on_delete=models.CASCADE)          
     tip = models.CharField(max_length=90)
     denumire = models.CharField(max_length=90)
     categorie = models.CharField(max_length=90)
-    judet_bucati = ListField()
+    judet_bucati = models.ManyToManyField(Region)
+
+class Region(models.Model):
+    judet = models.CharField(max_length=30)
+    bucati = models.IntegerField()
+    
