e94c80c23a302773cca8008044dfd6f9d64d6a50
Stefan Ilie <stefan.ilie@schindler.com>
2017-08-29 17:41:31 +0300

    connect to db and made db structure

--- hml/hml/settings.py
+++ hml/hml/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'howmanyleft.apps.HowmanyleftConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- hml/howmanyleft/models.py
+++ hml/howmanyleft/models.py
@@ -1,20 +1,19 @@
-from django.db import models
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-from djangotoolbox.fields import ListField, EmbeddedModelField
+from django.db import models
 # Create your models here.
 
 class Brand(models.Model):
     marca = models.CharField(max_length=90)
-    modele = ListField(EmbeddedModelField('Modele'))
-
-class Model(models.Model):
-    brand = models.ForeignKeyField(Brand, on_delete=models.CASCADE)          
-    tip = models.CharField(max_length=90)
-    denumire = models.CharField(max_length=90)
-    categorie = models.CharField(max_length=90)
-    judet_bucati = models.ManyToManyField(Region)
 
 class Region(models.Model):
     judet = models.CharField(max_length=30)
     bucati = models.IntegerField()
-    
+
+class Model(models.Model):
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    tip = models.CharField(max_length=90)
+    denumire = models.CharField(max_length=90)
+    categorie = models.CharField(max_length=90)
+    judet_bucati = models.ManyToManyField(Region)
